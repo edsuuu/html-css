fbe64b6ee080fab7a3b9ee7dca350fa965328458
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-09-27 22:07:59 -0300

    Del

--- reprodutor-de-musicas/src/assets/js/app.js
+++
@@ -1,7 +0,0 @@
-function playSong(song, songName) {
-    var audioPlayer =
-        document.getElementById("music-player");
-    var currentSong = document.getElementById("current-song");
-    audioPlayer.src = song;
-    currentSong.textContent = "Tocando: " + songName;
-}
